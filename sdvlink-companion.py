e322b6b7c0f4955adb891936e077613cbc30d1ed
Zubair Hamed <zubairhamed@gmail.com>
2024-02-20 22:50:33 +0100

    Additional Signals

--- sdvlink-companion.py
+++ sdvlink-companion.py
@@ -39,6 +39,7 @@ from colorama import Fore
 import calendar
 import time
 from datetime import datetime
+from timeit import default_timer as timer
 
 init(autoreset=True)
 
@@ -52,6 +53,11 @@ PATH_BEAM_LOW_ISON = "Vehicle.Body.Lights.Beam.Low.IsOn"
 PATH_BEAM_HIGH_ISON = "Vehicle.Body.Lights.Beam.High.IsOn"
 PATH_CURRENTGEAR = "Vehicle.Powertrain.Transmission.CurrentGear"
 PATH_BRAKEPEDAL_POSITION = "Vehicle.Chassis.Brake.PedalPosition"
+PATH_ISMOVING = "Vehicle.IsMoving"
+PATH_START_TIME = "Vehicle.StartTime"
+PATH_TRIP_DURATION = "Vehicle.TripDuration"
+PATH_EMERGENCY_BRAKE_DETECTED = "Vehicle.Chassis.Brake.IsDriverEmergencyBrakingDetected"
+PATH_PARKING_BRAKE_ENGAGED = "Vehicle.Chassis.ParkingBrake.IsEngaged"
 
 MAX_STEERING_LEFT = 40
 MAX_STEERING_RIGHT = -40
@@ -81,8 +87,15 @@ provisioningDict = {
     PATH_BEAM_LOW_ISON: False,
     PATH_BEAM_HIGH_ISON: False,
     PATH_CURRENTGEAR:GEAR_PARKED,
-    PATH_BRAKEPEDAL_POSITION: 0
+    PATH_BRAKEPEDAL_POSITION: 0,
+    PATH_ISMOVING: False,
+    PATH_START_TIME: "0000-01-01T00:00Z",
+    PATH_TRIP_DURATION: 0,
+    PATH_EMERGENCY_BRAKE_DETECTED: False,
+    PATH_PARKING_BRAKE_ENGAGED: True
 }
+
+tripStart = timer()
 
 def log(msg):
     current_GMT = time.gmtime()
@@ -90,8 +103,8 @@ def log(msg):
     dt = datetime.fromtimestamp(ts)
     print(f"{Fore.GREEN} {dt} {msg}")
 
-def kbMessage(kb, path, val):
-    log(f"{Fore.BLUE} [{kb}] {Fore.CYAN}{path} -> {Fore.RED}{val}")
+def logSetMessage(path, val):
+    log(f"{Fore.CYAN}{path} -> {Fore.RED}{val}")
 
 def logError(msg):
     log(f"{Fore.RED} {msg}")
@@ -104,24 +117,32 @@ def logInfo(msg):
     m = Fore.CYAN + msg
     log(m)
 
-def logCarEngineTurnOn():
-    logWarn("Car engines are off. Turn on car engine (Q) first")
+def allowedToMove():
+    engineOn = valueMap[PATH_ENGINE_RUNNING]
+    parkingBrakeOn = valueMap[PATH_PARKING_BRAKE_ENGAGED]
 
-async def Set(path, val, valType, kb):
+    if not engineOn:
+        logWarn("Car engines are off. Turn on car engine (Q) first")
+        return False
+    
+    if parkingBrakeOn:
+        logWarn("Parking Brake is Engaged. Disengage (R) before moving off")
+        return False
+    
+    return True
+
+async def Set(path, val, valType):
     try:
         async with vssClient:
             entry = EntryUpdate(DataEntry(path, value=Datapoint(value=val), metadata=Metadata(data_type=valType)), (VssField.VALUE,))
             await vssClient.set(updates=(entry,))
-            if kb is not None:
-                kbMessage(kb, path, val)
+            logSetMessage(path, val)
     except Exception as err:
         logError(f"ERROR: Unable to connect to Kuksa Databroker {err}. Connection Details: {DATABROKER_ADDRESS} port {DATABROKER_PORT}")
 
 async def handleAccelerate():
     """ Accelerate car. If car was in N or R gear and speed goes above 0, automatically goes into D """
-    engineOn = valueMap[PATH_ENGINE_RUNNING]
-    if not engineOn:        
-        logCarEngineTurnOn()
+    if not allowedToMove():
         return
 
     originalSpeed = valueMap[PATH_VEHICLE_SPEED]
@@ -130,6 +151,8 @@ async def handleAccelerate():
     if newSpeed == 0:
         # We are stationary, so set car to neutral
         await handleGearNeutral()
+        await Set(PATH_ISMOVING, False, DataType.BOOLEAN)
+        return
 
     if newSpeed > 0 and originalSpeed <= 0:
         # If previous speed was negative and we went into positive speed, go into drive gear
@@ -137,84 +160,95 @@ async def handleAccelerate():
 
     if newSpeed > 0:
         # Increase speed and set gear
-        await Set(PATH_VEHICLE_SPEED, newSpeed, DataType.FLOAT, "S")
+        await Set(PATH_ISMOVING, True, DataType.BOOLEAN)
+        await Set(PATH_VEHICLE_SPEED, newSpeed, DataType.FLOAT)
     else:
         # Decelerate and set brake position
-        await Set(PATH_BRAKEPEDAL_POSITION, 50, DataType.UINT8, "S")    
-        await Set(PATH_VEHICLE_SPEED, newSpeed, DataType.FLOAT, "S")
-        await Set(PATH_BRAKEPEDAL_POSITION, 0, DataType.UINT8, "S")
+        await Set(PATH_ISMOVING, True, DataType.BOOLEAN)
+        await Set(PATH_BRAKEPEDAL_POSITION, 50, DataType.UINT8)    
+        await Set(PATH_VEHICLE_SPEED, newSpeed, DataType.FLOAT)
+        await Set(PATH_BRAKEPEDAL_POSITION, 0, DataType.UINT8)
 
 async def handleDecelerate():
     """ Decelerates car. If car was in N or D gear and speed goes below 0, automatically goes into R """
-    engineOn = valueMap[PATH_ENGINE_RUNNING]
-    if not engineOn:
-        logCarEngineTurnOn()
+    if not allowedToMove():
         return
 
     originalSpeed = valueMap[PATH_VEHICLE_SPEED]
     newSpeed = max(originalSpeed - SPEED_INCREMENT, -MAX_SPEED)
 
-    if newSpeed == 0:
+    if newSpeed == 0:        
         # We are stationary, so set car to neutral
         await handleGearNeutral()
+        await Set(PATH_ISMOVING, False, DataType.BOOLEAN)
+        return
 
-    await Set(PATH_VEHICLE_SPEED, newSpeed, DataType.FLOAT, "S")
-
+    await Set(PATH_ISMOVING, True, DataType.BOOLEAN)
     if newSpeed < 0 and originalSpeed >= 0:
         # If previous speed was positive and now we're into negative, go into reverse gear
+        await Set(PATH_VEHICLE_SPEED, newSpeed, DataType.FLOAT)
         await handleGearReverse()
     elif newSpeed >= 0:
         # Pump brakes if in positive speed to decelerate
-        await Set(PATH_BRAKEPEDAL_POSITION, 50, DataType.UINT8, "S")    
-        await Set(PATH_BRAKEPEDAL_POSITION, 0, DataType.UINT8, "S")
-
+        await Set(PATH_BRAKEPEDAL_POSITION, 50, DataType.UINT8)
+        await Set(PATH_VEHICLE_SPEED, newSpeed, DataType.FLOAT)
+        await Set(PATH_BRAKEPEDAL_POSITION, 0, DataType.UINT8)
 
 async def handleLeftTurn():
     """ Axle turn Left to turn car. VSS Values are Positive """
-    engineOn = valueMap[PATH_ENGINE_RUNNING]
-    if not engineOn:
-        logCarEngineTurnOn()
+    if not allowedToMove():
         return
 
     turnAngle = min(valueMap[PATH_STEERING_ANGLE] + STEERING_INCREMENT, MAX_STEERING_LEFT)
-    await Set(PATH_STEERING_ANGLE, turnAngle, DataType.FLOAT, "A")
+    await Set(PATH_STEERING_ANGLE, turnAngle, DataType.FLOAT)
 
 async def handleRightTurn():    
     """ Axle turn Right to turn car. VSS Values are Negative """
-    engineOn = valueMap[PATH_ENGINE_RUNNING]
-    if not engineOn:
-        logCarEngineTurnOn()
+    if not allowedToMove():
         return
 
     turnAngle = max(valueMap[PATH_STEERING_ANGLE] - STEERING_INCREMENT, MAX_STEERING_RIGHT)
-    await Set(PATH_STEERING_ANGLE, turnAngle, DataType.FLOAT, "D")
+    await Set(PATH_STEERING_ANGLE, turnAngle, DataType.FLOAT)
 
 async def handleLeftSignal():
     """ Toggles Left Turn Indicator """
     leftSignal = not valueMap[PATH_LEFTINDICATOR_SIGNALING]
-    await Set(PATH_LEFTINDICATOR_SIGNALING, leftSignal, DataType.BOOLEAN, "SHIFT+A")
+    await Set(PATH_LEFTINDICATOR_SIGNALING, leftSignal, DataType.BOOLEAN)
 
 async def handleRightSignal():
     """ Toggles Right Turn Indicator """
     rightSignal = not valueMap[PATH_RIGHTINDICATOR_SIGNALING]
-    await Set(PATH_RIGHTINDICATOR_SIGNALING, rightSignal, DataType.BOOLEAN, "SHIFT+D")
+    await Set(PATH_RIGHTINDICATOR_SIGNALING, rightSignal, DataType.BOOLEAN)
 
-async def handleEnginePower():
+async def handleEnginePower():    
     """ Toggles Engine """
-    engineOn = not valueMap[PATH_ENGINE_RUNNING]
+    global tripStart
 
+    engineOn = not valueMap[PATH_ENGINE_RUNNING]
+    if engineOn:
+        current_GMT = time.gmtime()
+        ts = calendar.timegm(current_GMT)
+        dt = datetime.fromtimestamp(ts)
+        await Set(PATH_START_TIME, f"{dt}", DataType.STRING)
+        await Set(PATH_TRIP_DURATION, 0, DataType.FLOAT)
+        tripStart = timer()
+    else:
+        tripEnd = timer()
+        tripTime = tripEnd - tripStart
+        await Set(PATH_TRIP_DURATION, tripTime, DataType.FLOAT)
+    
     # TODO: If speed is positive, decelerate speed over time
-    await Set(PATH_ENGINE_RUNNING, engineOn, DataType.BOOLEAN, "Q")
+    await Set(PATH_ENGINE_RUNNING, engineOn, DataType.BOOLEAN)
 
 async def handleLowBeam():
     """ Toggle Low Beam Light """
     lowBeamOn = not valueMap[PATH_BEAM_LOW_ISON]
-    await Set(PATH_BEAM_LOW_ISON, lowBeamOn, DataType.BOOLEAN, "L")
+    await Set(PATH_BEAM_LOW_ISON, lowBeamOn, DataType.BOOLEAN)
 
 async def handleHighBeam():
     """ Toggle High Beam Light """
     highBeamOn = not valueMap[PATH_BEAM_HIGH_ISON]
-    await Set(PATH_BEAM_HIGH_ISON, highBeamOn, DataType.BOOLEAN, "SHIFT+L")
+    await Set(PATH_BEAM_HIGH_ISON, highBeamOn, DataType.BOOLEAN)
 
 async def unimplemented():
     print("Not implemented yet")
@@ -235,7 +269,6 @@ async def subscribe():
             for update in updates:
                 if update.entry.value is not None:
                     valueMap[update.entry.path] = update.entry.value.value
-                    # kbMessage("SUB", update.entry.path, update.entry.value.value)
 
 def provisionValue(entries, path, defaultValue):
     """ Sets up default values for a given entry """
@@ -268,57 +301,74 @@ async def handleGearPark():
     engineOn = valueMap[PATH_ENGINE_RUNNING]
     currentSpeed = valueMap[PATH_VEHICLE_SPEED]
 
-    if not engineOn or currentSpeed > 0:
+    if not allowedToMove() or currentSpeed > 0:
         if not engineOn:
-            logCarEngineTurnOn()
+            return
         else:
             logWarn("Unable to shift to Park. Decelerate to a stop before putting into Park")
         return
 
     logInfo("Shifted to Gear: Parked")
-    await Set(PATH_CURRENTGEAR, GEAR_PARKED, DataType.INT8, "1")
+    await Set(PATH_CURRENTGEAR, GEAR_PARKED, DataType.INT8)
 
 async def handleGearReverse():
-    engineOn = valueMap[PATH_ENGINE_RUNNING]
     currentSpeed = valueMap[PATH_VEHICLE_SPEED]
 
-    if not engineOn:
-        logCarEngineTurnOn()
+    if not allowedToMove():
+        return
     elif currentSpeed <= 0:
         logInfo("Shifted to Gear: Reverse")
-        await Set(PATH_CURRENTGEAR, GEAR_REVERSE, DataType.INT8, "R")
+        await Set(PATH_CURRENTGEAR, GEAR_REVERSE, DataType.INT8)
     else:
         logWarn("Unable to shift to Reverse. Decelerate to a stop before shifting")
 
 async def handleGearNeutral():
-    engineOn = valueMap[PATH_ENGINE_RUNNING]
-
-    if not engineOn:
-        logCarEngineTurnOn()
+    if not allowedToMove():
         return
 
     logInfo("Shifted to Gear: Neutral")    
-    await Set(PATH_CURRENTGEAR, GEAR_NEUTRAL, DataType.INT8, "2")
+    await Set(PATH_CURRENTGEAR, GEAR_NEUTRAL, DataType.INT8)
 
 async def handleGearDrive():
-    engineOn = valueMap[PATH_ENGINE_RUNNING]
-
-    if not engineOn:
-        logCarEngineTurnOn()
+    if not allowedToMove():
         return
 
     logInfo("Shifted to Gear: Drive")
-    await Set(PATH_CURRENTGEAR, GEAR_DRIVE, DataType.INT8, "E")
+    await Set(PATH_CURRENTGEAR, GEAR_DRIVE, DataType.INT8)
 
 async def handleGearManual():
-    engineOn = valueMap[PATH_ENGINE_RUNNING]
-
-    if not engineOn:
-        logCarEngineTurnOn()
+    if not allowedToMove():
         return
 
     logInfo("Shifted to Gear: Manual")
-    await Set(PATH_CURRENTGEAR, GEAR_MANUAL, DataType.INT8, "3")
+    await Set(PATH_CURRENTGEAR, GEAR_MANUAL, DataType.INT8)
+
+async def handleBraking():
+    if not allowedToMove():
+        return
+        
+    handleDecelerate()
+
+async def handleEmergencyBraking():
+    if not allowedToMove():
+        return
+    
+    currentSpeed = valueMap[PATH_VEHICLE_SPEED]
+    if currentSpeed == 0:
+        return    
+
+    # Emit Vehicle.Chassis.Brake.IsDriverEmergencyBrakingDetected True
+    # TODO Reduce Speed till 0 aggressively
+    # Emit Vehicle.Chassis.Brake.IsDriverEmergencyBrakingDetected False
+    pass
+
+async def handleEngageParkingBrake():
+    parkingBrakeOn = not valueMap[PATH_PARKING_BRAKE_ENGAGED]
+    
+    if parkingBrakeOn:
+        await Set(PATH_PARKING_BRAKE_ENGAGED, True, DataType.BOOLEAN)
+    else:
+        await Set(PATH_PARKING_BRAKE_ENGAGED, False, DataType.BOOLEAN)
 
 # Keyboard Bindings
 keyboard.add_hotkey('Q', lambda: asyncio.run(handleEnginePower())) # Turn off and On Engine
@@ -333,8 +383,11 @@ keyboard.add_hotkey('SHIFT+L', lambda: asyncio.run(handleHighBeam())) # High Bea
 keyboard.add_hotkey('1', lambda: asyncio.run(handleGearPark())) # Gear: Park
 keyboard.add_hotkey('2', lambda: asyncio.run(handleGearNeutral())) # Gear: Neutral
 keyboard.add_hotkey('3', lambda: asyncio.run(handleGearManual())) # Gear: Sport/Manual
-keyboard.add_hotkey('R', lambda: asyncio.run(handleGearReverse())) # Gear: Reverse
-keyboard.add_hotkey('E', lambda: asyncio.run(handleGearDrive())) # Gear: Drive
+keyboard.add_hotkey('4', lambda: asyncio.run(handleGearReverse())) # Gear: Reverse
+keyboard.add_hotkey('5', lambda: asyncio.run(handleGearDrive())) # Gear: Drive
+keyboard.add_hotkey('E', lambda: asyncio.run(handleEngageParkingBrake())) # Parking Brake
+keyboard.add_hotkey('space', lambda: asyncio.run(handleBraking())) # Braking
+keyboard.add_hotkey('shift+space', lambda: asyncio.run(handleEmergencyBraking())) # Braking
 
 print(f""" {Fore.CYAN}
    _____ _______      ___      _       _    
@@ -361,12 +414,12 @@ print(f""" {Fore.CYAN}
       {Fore.YELLOW}| {Fore.RED}1        {Fore.YELLOW}| {Fore.RED}gear park         {Fore.YELLOW}|
       {Fore.YELLOW}| {Fore.RED}2        {Fore.YELLOW}| {Fore.RED}gear neutral      {Fore.YELLOW}|
       {Fore.YELLOW}| {Fore.RED}3        {Fore.YELLOW}| {Fore.RED}gear sport/manual {Fore.YELLOW}|
-      {Fore.YELLOW}| {Fore.RED}R        {Fore.YELLOW}| {Fore.RED}gear reverse      {Fore.YELLOW}|
-      {Fore.YELLOW}| {Fore.RED}E        {Fore.YELLOW}| {Fore.RED}gear drive        {Fore.YELLOW}|
+      {Fore.YELLOW}| {Fore.RED}4        {Fore.YELLOW}| {Fore.RED}gear reverse      {Fore.YELLOW}|
+      {Fore.YELLOW}| {Fore.RED}5        {Fore.YELLOW}| {Fore.RED}gear drive        {Fore.YELLOW}|
       {Fore.YELLOW} ------------------------------ 
       
 """)
 asyncio.run(provisionVehicleValues())
 asyncio.run(subscribe())
 
-keyboard.wait()
+keyboard.wait('X')
